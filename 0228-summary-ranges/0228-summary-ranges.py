83c7000e0a4a2f347687177a8dfd7740c0dce1e7
MayureshSawant9 <122062201+MayureshSawant9@users.noreply.github.com>
2025-05-11 00:44:27 +0530

    Time: 0 ms (100%), Space: 17.8 MB (33.44%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,50 +1,22 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
 
-        def single_print(number: int) -> str:
-            return f'{number}'
-
-        def double_print(num1, num2: int) -> str:
-            return f'{num1}->{num2}' 
-
-        if len(nums) == 0:
+        if not nums:
             return []
-        
-        elif len(nums) == 1:
-            return [str(nums[0])]
-
-        elif len(nums) == 2:
-            if nums[1] == nums[0] + 1:
-                return [double_print(nums[0], nums[1])]
-            else:
-                return [single_print(nums[0]), single_print(nums[1])]
 
         result = []
+        start = nums[0]
+        
+        for i in range(1, len(nums) + 1):
 
-        start, i = 0, 0
+            if i == len(nums) or nums[i] != nums[i-1] + 1:
 
-        while True:
-
-            if nums[i+1] == nums[i] + 1:
-                
-                i += 1
-
-            else:
-
-                if i == start:
-                    result.append(single_print(nums[start]))
+                if start == nums[i-1]:
+                    result.append(str(start))
                 else:
-                    result.append(double_print(nums[start], nums[i]))
-                    
-                i += 1
-                start = i
-            
-            if i == len(nums) - 1:
-                if i == start:
-                    result.append(single_print(nums[start]))
-                else:
-                    result.append(double_print(nums[start], nums[i]))
-
-                return result
+                    result.append(f'{start}->{nums[i-1]}')
                 
+                if i < len(nums):
+                    start = nums[i]
 
+        return result
