be7f6e18c33b16ee7e3d0d77678014ee30965159
MayureshSawant9 <122062201+MayureshSawant9@users.noreply.github.com>
2025-05-11 01:14:02 +0530

    Time: 0 ms (100%), Space: 3.9 MB (31.95%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.go
+++ 0228-summary-ranges/0228-summary-ranges.go
@@ -1,27 +1,28 @@
-import "fmt"
+import (
+    "fmt"
+    "strconv"
+)
 func summaryRanges(nums []int) []string {
+	if len(nums) == 0 {
+		return []string{}
+	}
 
-    if len(nums) == 0 {
-        return []string{}
-    }
+	result := []string{}
+	start := nums[0]
 
-    result := []string{}
-    start := nums[0]
-
-    for i := 1; i < len(nums) + 1; i++ {
-        if i == len(nums) || nums[i] != nums[i-1] + 1{
-            
-            if start == nums[i-1] {
-                result = append(result, fmt.Sprint(start))
-            } else {
-                result = append(result, fmt.Sprintf("%v->%v", start, nums[i-1]))
-            }
-
-            if i < len(nums) {
-                start = nums[i]
-            }
-        }
-    } 
-    return result
-
+	for i := 1; i <= len(nums); i++ { // loop until the end
+		if i == len(nums) || nums[i] != nums[i-1]+1 {
+			// Check if the current range is just a single number
+			if start == nums[i-1] {
+				result = append(result, strconv.Itoa(start)) // Use strconv for int to string conversion
+			} else {
+				result = append(result, fmt.Sprintf("%d->%d", start, nums[i-1])) // Use fmt.Sprintf for ranges
+			}
+			// If we haven't reached the end, update start for the next range
+			if i < len(nums) {
+				start = nums[i]
+			}
+		}
+	}
+	return result
 }
