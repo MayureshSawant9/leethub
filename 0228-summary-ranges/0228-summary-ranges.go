58c30152dc854ac1bc9734709cd85dc1938371be
MayureshSawant9 <122062201+MayureshSawant9@users.noreply.github.com>
2025-05-11 01:10:01 +0530

    Time: 0 ms (100%), Space: 3.9 MB (31.95%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.go
@@ -0,0 +1,27 @@
+import "fmt"
+func summaryRanges(nums []int) []string {
+
+    if len(nums) == 0 {
+        return []string{}
+    }
+
+    result := []string{}
+    start := nums[0]
+
+    for i := 1; i < len(nums) + 1; i++ {
+        if i == len(nums) || nums[i] != nums[i-1] + 1{
+            
+            if start == nums[i-1] {
+                result = append(result, fmt.Sprint(start))
+            } else {
+                result = append(result, fmt.Sprintf("%v->%v", start, nums[i-1]))
+            }
+
+            if i < len(nums) {
+                start = nums[i]
+            }
+        }
+    } 
+    return result
+
+}
