97829d9d3b4e073ce6236f01b95c956e68c637d0
MayureshSawant9 <122062201+MayureshSawant9@users.noreply.github.com>
2025-05-18 00:12:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (80.99%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.go
@@ -0,0 +1,16 @@
+func majorityElement(nums []int) int {
+    var candidate int
+    count := 0
+
+    for _, num := range nums {
+        if count ==0 {
+            candidate = num
+        }
+        if candidate == num {
+            count++ 
+        } else {
+            count--
+        }
+    }
+    return candidate
+}
