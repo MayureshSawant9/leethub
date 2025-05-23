c087c85e331c92c92631c3a95a1805cd334d4d4f
MayureshSawant9 <122062201+MayureshSawant9@users.noreply.github.com>
2025-05-23 16:19:28 +0530

    Time: 11 ms (15.87%), Space: 6 MB (30.98%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -0,0 +1,18 @@
+import "regexp"
+func isPalindrome(s string) bool {
+    pattern := regexp.MustCompile("[^A-Za-z0-9]")
+    phrase := pattern.ReplaceAllString(s, "")
+    phrase = strings.ToLower(phrase)
+
+    l, r := 0, len(phrase) - 1
+    fmt.Print(phrase)
+    for l < r {
+        if phrase[l] != phrase[r]{
+            return false
+        }
+        l++
+        r--
+    }
+
+    return true
+}
