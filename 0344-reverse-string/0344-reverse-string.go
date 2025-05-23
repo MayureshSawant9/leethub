e059bd0f6122e55faed619519ace73a25964a387
MayureshSawant9 <122062201+MayureshSawant9@users.noreply.github.com>
2025-05-23 14:55:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (29.64%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.go
+++ 0344-reverse-string/0344-reverse-string.go
@@ -1,7 +1,17 @@
 func reverseString(s []byte)  {
-    n := len(s)
+    // n := len(s)
 
-    for i:=0; i<n/2; i++ {
-        s[i], s[n-i-1] = s[n-i-1], s[i]
+    // for i:=0; i<n/2; i++ {
+    //     s[i], s[n-i-1] = s[n-i-1], s[i]
+    // }
+
+    // two pointer
+    l, r := 0, len(s) - 1
+
+    for l < r {
+        s[l], s[r] = s[r], s[l]
+        l++
+        r--
     }
+
 }
